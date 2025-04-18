c12ead72d20c4b7fb7f6bab4c24ff07a0ab69bde
Sakshi Patil <sakshipatil5333@gmail.com>
2025-04-18 10:13:50 +0530

    JAVA codes

--- chapterRecursion/BinaryStringProblem.java
+++ chapterRecursion/BinaryStringProblem.java
@@ -1,3 +1,15 @@
 public class BinaryStringProblem {
-    
+    public static void binaryString(int n, String str,int privious){
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        binaryString(n-1, str+"0", 0);
+        if(privious==0){
+            binaryString(n-1, str+"1", 1);
+        }
+    }
+    public static void main(String[] args){
+        binaryString(2, "", 0);
+    }
 }
